0d13449071d8c2a40f03ce5c973dfbd105992e90
011800466 <011800466@prof.uninassau.edu.br>
2024-12-26 23:44:34 +0800

    修改数据，跑起来

--- src/main/java/com/example/wuyou/constant/WXConfigConstant.java
+++ src/main/java/com/example/wuyou/constant/WXConfigConstant.java
@@ -3,7 +3,7 @@ package com.example.wuyou.constant;
 public interface WXConfigConstant {
     String CODE_URL = "https://api.weixin.qq.com/sns/jscode2session?appid=";
 
-    String APP_ID = "你的APP_ID";
+    String APP_ID = "wx42e5ff9a1f0757ed";
 
-    String SECRET = "你的密钥";
+    String SECRET = "cbf75f464684b1e323a97491c72c2bfd";
 }
